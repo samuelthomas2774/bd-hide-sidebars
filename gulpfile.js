5b51c634537c41a8cc45d2be3aa29f4d82d2e18f
Samuel Elliott <samuel@fancy.org.uk>
2018-07-08 00:09:51 +0100

    Add .editorconfig and move all build tasks to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,46 @@
 const gulp = require('gulp');
+const pump = require('pump');
 const rename = require('gulp-rename');
 const inject = require('gulp-inject-string');
+const sass = require('gulp-sass');
 
 const config = require('./config');
 
-const header_meta = {
-    name: config.info.id,
-    description: config.info.description,
-    author: config.info.authors.map(a => a.name).join(', '),
-    version: config.info.version
-};
-const header = `//META${JSON.stringify(header_meta)}*//\n`;
+gulp.task('build', function () {
+    return pump([
+        gulp.src('src/hide-sidebars.scss'),
+        rename('hide-sidebars.css'),
+        sass({
+            sourceMapContents: true,
+            sourceMapEmbed: true
+        }),
+        gulp.dest('dist')
+    ]);
+});
+
+gulp.task('watch', gulp.series('build', function () {
+    return gulp.watch('src/**/*.scss', gulp.series('build'));
+}));
 
 gulp.task('release', function () {
-    return gulp.src('dist/hide-sidebars.min.css')
-        .pipe(rename('hide-sidebars.theme.css'))
-        .pipe(inject.prepend(header))
-        .pipe(gulp.dest('dist/'));
+    const header_meta = {
+        name: config.info.id,
+        description: config.info.description,
+        author: config.info.authors.map(a => a.name || a).join(', '),
+        version: config.info.version
+    };
+
+    const header = `//META${JSON.stringify(header_meta)}*//\n`;
+
+    return pump([
+        gulp.src('src/hide-sidebars.scss'),
+
+        rename('hide-sidebars.min.css'),
+        sass({ outputStyle: 'compressed' }),
+        gulp.dest('dist'),
+
+        rename('hide-sidebars.theme.css'),
+        inject.prepend(header),
+        gulp.dest('dist')
+    ]);
 });
